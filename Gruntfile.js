f9a33510fb41843490def26ca6be0969a2d61ac9
songzigw <songzigw@163.com>
2016-08-23 17:48:03 +0800

    js

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,39 +1,16 @@
-module.exports = function(grunt){
-	grunt.initConfig({
-        pkg : grunt.file.readJSON('package.json'),
-		concat : {
-            'dist/sog.debug.js' :
-                ['src/js/sog.js','src/js/module/*.js','src/js/plugin/*.js'],
-            'dist/sog.css' : ['src/css/base.css','src/css/module/*.css']
-		},
-        uglify : {
-            target : {
-                files : {
-                    'dist/sog.min.js': 'dist/sog.debug.js'
-                }
-            }
-        },
-        cssmin : {
-            target : {
-                files : {
-                    'dist/sog.min.css': 'dist/sog.css'
-                }
-            }
-        },
-        copy : {
-            target : {
-                files : [
-                    {expand: true,cwd: 'dist/',src: ['sog.debug.js'],dest: 'demo/js/lib/'},
-                    {expand: true,cwd: 'dist/',src: ['sog.css'],dest: 'demo/css/'}
-                ]
-            }
-        }
+module.exports = function(grunt) {
+    
+    'use strict'
+    
+    grunt.initConfig({
+        pkg : grunt.file.readJSON('package.json')
 
-	});
-  	grunt.loadNpmTasks('grunt-contrib-concat');
+    });
+
+    grunt.loadNpmTasks('grunt-contrib-concat');
     grunt.loadNpmTasks('grunt-contrib-uglify');
     grunt.loadNpmTasks('grunt-contrib-cssmin');
     grunt.loadNpmTasks('grunt-contrib-copy');
 
-  	grunt.registerTask('default', ['concat','uglify','cssmin','copy']);
+    grunt.registerTask('default', [ 'concat', 'uglify', 'cssmin', 'copy' ]);
 }
--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 /*!
  * @auth zhangsong
- * @date: 2016年3月24日
+ * @date 2016年3月24日
  */
 var http = require('http');
 var path = require('path');
@@ -8,11 +8,11 @@ var urlparse = require('url').parse;
 var express = require('express');
 
 var app = express();
-app.use("/proxy", function (req, res) {
+app.use("/proxy", function(req, res) {
     var url = req.url.substr(5);
     var target = urlparse(url);
     var headers = {};
-    for (var k in req.headers) {
+    for ( var k in req.headers) {
         if (k === 'host' || k === 'connection') {
             continue;
         }
@@ -21,33 +21,33 @@ app.use("/proxy", function (req, res) {
     console.log(url);
     console.log(target.path);
     var options = {
-        host: target.hostname,
-        port: target.port || 80,
-        path: target.path,
-        method: req.method,
-        headers: headers
+        host : target.hostname,
+        port : target.port || 80,
+        path : target.path,
+        method : req.method,
+        headers : headers
     };
 
-    var proxyReq = http.request(options, function (response) {
+    var proxyReq = http.request(options, function(response) {
         res.writeHead(response.statusCode, response.headers);
-        response.on('data', function (chunk) {
+        response.on('data', function(chunk) {
             res.write(chunk);
         });
-        response.on('end', function () {
+        response.on('end', function() {
             res.end();
         });
     });
 
-    proxyReq.on('error', function (err) {
+    proxyReq.on('error', function(err) {
         proxyReq.abort();
         res.writeHead(500);
         res.end(url + ' error: ' + err.message);
     });
 
-    req.on('data', function (chunk) {
+    req.on('data', function(chunk) {
         proxyReq.write(chunk);
     });
-    req.on('end', function () {
+    req.on('end', function() {
         proxyReq.end();
     });
 });
